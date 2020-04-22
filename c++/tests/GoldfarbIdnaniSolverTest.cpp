7dfd327e9f29caf99cfdeb2bf8a8187cb2427cba
Adrien Escande <adrien.escande@aist.go.jp>
2020-04-22 17:54:50 +0900

    Extend tests of GoldfarbIdnaniSolver

--- c++/tests/GoldfarbIdnaniSolverTest.cpp
+++ c++/tests/GoldfarbIdnaniSolverTest.cpp
@@ -8,8 +8,10 @@
 #include "doctest/doctest.h"
 
 #include <jrl-qp/GoldfarbIdnaniSolver.h>
+#include "utils.h"
 
 using namespace Eigen;
+using namespace jrlqp;
 
 TEST_CASE("Simple problem")
 {
@@ -21,16 +23,40 @@ TEST_CASE("Simple problem")
   VectorXd xl(0);
   VectorXd xu(0);
 
-  jrlqp::GoldfarbIdnaniSolver qp(3, 5, false);
+  GoldfarbIdnaniSolver qp(3, 5, false);
+
+  // unconstraint solution
   qp.solve(G, a, C, bl, bu, xl, xu);
 
-  std::cout << qp.solution().transpose() << std::endl;
-  std::cout << qp.multipliers() << std::endl;
+  G.setIdentity();
+  FAST_CHECK_UNARY(test::testKKT(qp.solution(), qp.multipliers(), G, a, C, bl, bu, xl, xu, true));
 
+  // At least one constraint activated
   bl[1] = -2; bu[1] = -1;
   G.setIdentity();
   qp.solve(G, a, C, bl, bu, xl, xu);
 
-  std::cout << qp.solution().transpose() << std::endl;
-  std::cout << qp.multipliers() << std::endl;
+  G.setIdentity();
+  FAST_CHECK_UNARY(test::testKKT(qp.solution(), qp.multipliers(), G, a, C, bl, bu, xl, xu, true));
+}
+
+TEST_CASE("Simple problem paper")
+{
+  // example from the Goldfarb-Idnani paper
+  MatrixXd G(2, 2); G << 4, -2, -2, 4;
+  VectorXd a(2); a << 6, 0;
+  MatrixXd C(2, 1); C << 1, 1;
+  VectorXd bl(1); bl << 2;
+  VectorXd bu(1); bu << 10;
+  VectorXd xl = VectorXd::Zero(2);
+  VectorXd xu = VectorXd::Constant(2, 10);
+
+  GoldfarbIdnaniSolver qp(2, 1, true);
+  qp.options(SolverOptions().logFlags(0xFFFF));
+
+  // unconstraint solution
+  qp.solve(G, a, C, bl, bu, xl, xu);
+
+  G << 4, -2, -2, 4;
+  FAST_CHECK_UNARY(test::testKKT(qp.solution(), qp.multipliers(), G, a, C, bl, bu, xl, xu, true));
 }
